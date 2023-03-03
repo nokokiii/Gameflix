7f8c52a2eecfa5ec42e7ba377a6b248ccdf7e6b9
Nokokiii <kubanokoki@gmail.com>
2023-03-03 09:24:46 +0100

    Config
    Uploading essential files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '../app.css';
+    import Component from './component.svelte'
+</script>
+
+
+<Component/>
---
+++ src/routes/component.svelte
@@ -0,0 +1,5 @@
+<script>
+    let a = 2
+</script>
+
+<h1>hello {a} world</h1>
