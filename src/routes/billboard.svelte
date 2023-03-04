69190ce48eae1335dbe7a2f47be263e6df96f82c
nokokiii <kubanokoki@gmail.com>
2023-03-04 14:53:03 +0100

    billboard

---
+++ src/routes/billboard.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="billboard">
+  <h1>proponowana gra</h1>
+</div>
+
+<style>
+  h1 {
+    color: red;
+  }
+
+  .billboard {
+    width: 100%;
+    height: 60vh;
+    background-color: #f1f1f1;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
